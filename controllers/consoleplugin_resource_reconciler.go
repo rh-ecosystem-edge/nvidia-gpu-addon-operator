b8618f738c54b9bbd402dccd32b92a19d6af729b
Michail Resvanis <mresvani@redhat.com>
2022-05-31 14:51:39 +0200

    Add Console Plugin NVIDIA GPU resource requests/limits
    Signed-off-by: Michail Resvanis <mresvani@redhat.com>

--- controllers/consoleplugin_resource_reconciler.go
+++ controllers/consoleplugin_resource_reconciler.go
@@ -26,6 +26,7 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
@@ -363,6 +364,16 @@ func (r *ConsolePluginResourceReconciler) setDesiredConsolePluginDeployment(
 		{
 			ContainerPort: 9443,
 			Protocol:      corev1.ProtocolTCP,
+		},
+	}
+	consolePluginContainer.Resources = corev1.ResourceRequirements{
+		Requests: map[corev1.ResourceName]resource.Quantity{
+			corev1.ResourceCPU:    resource.MustParse("10m"),
+			corev1.ResourceMemory: resource.MustParse("100Mi"),
+		},
+		Limits: map[corev1.ResourceName]resource.Quantity{
+			corev1.ResourceCPU:    resource.MustParse("20m"),
+			corev1.ResourceMemory: resource.MustParse("200Mi"),
 		},
 	}
 
